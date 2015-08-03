d98e366602912137b82046f43a3c3dcb6e9ccc0b
Marcelo Cuellar <mcuellar@buyerzone.com>
2015-08-02 23:53:00 -0400

    Product initial betterman page.

---
+++ src/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace interceuticals_responsive.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Betterman()
+        {
+            return View();
+        }
+    }
+}
